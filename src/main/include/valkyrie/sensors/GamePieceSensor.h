bf63db02fef499935806c9938c6c55cca8c50abe
Mau38 <maudiaz0109@gmail.com>
2024-12-21 16:48:25 -0600

    feat(Game Piece): Added homography matrix Adding test cases for homography matrix

--- src/main/include/valkyrie/sensors/GamePieceSensor.h
+++ src/main/include/valkyrie/sensors/GamePieceSensor.h
@@ -1,10 +1,14 @@
+#include "Eigen/Core"
 #include "frc/estimator/SwerveDrivePoseEstimator.h"
 #include "units/length.h"
 #include "valkyrie/sensors/VisionSensor.h"
 
 namespace valor {
 
+typedef Eigen::Matrix<double, 3, 3> RotationMatrix;
 
+//TODO: Take input for the camera type
+//TODO: Take input for the camera's intrinsic matrix
 class GamePieceSensor : public valor::VisionSensor {
     public:
         GamePieceSensor(frc::TimedRobot* robot, const char *name, frc::Pose3d _cameraPose, frc::SwerveDrivePoseEstimator<4>* estimator);
@@ -19,6 +23,8 @@ class GamePieceSensor : public valor::VisionSensor {
         void updateRelative();
         void updateRelativeToCenter();
 
+        RotationMatrix rollMatrix, yawMatrix, pitchMatrix, fullRotationMatrix;
+        Eigen::Matrix<double, 3, 1> cameraTranslation;
         frc::SwerveDrivePoseEstimator<4>* estimator;
 };
 }
