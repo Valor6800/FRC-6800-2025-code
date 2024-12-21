33d6115e519a60cdf40bd681efdb0acfa0a96541
Mau38 <maudiaz0109@gmail.com>
2024-12-21 16:48:25 -0600

    feat(Game Piece): Added homography matrix Adding test cases for homography matrix

--- src/main/cpp/valkyrie/sensors/GamePieceSensor.cpp
+++ src/main/cpp/valkyrie/sensors/GamePieceSensor.cpp
@@ -1,4 +1,5 @@
 #include "valkyrie/sensors/GamePieceSensor.h"
+#include "Eigen/Core"
 #include "frc/geometry/Pose2d.h"
 #include "frc/geometry/Pose3d.h"
 #include "units/angle.h"
@@ -14,10 +15,39 @@ GamePieceSensor::GamePieceSensor(frc::TimedRobot* robot, const char *name, frc::
     estimator(estimator)
 {
     setGetter([this](){return getGlobalPose();});
+
+    //TODO: Make these constants
+    rollMatrix = RotationMatrix{
+        {cos(cameraPose.Rotation().X().value()), -sin(cameraPose.Rotation().X().value()), 0},
+        {sin(cameraPose.Rotation().X().value()), cos(cameraPose.Rotation().X().value()), 0},
+        {0, 0, 1}
+    };
+
+    //TODO: Make these constants
+    yawMatrix = RotationMatrix{
+        {1, 0, 0},
+        {0, cos(cameraPose.Rotation().Z().value()), -sin(cameraPose.Rotation().Z().value())},
+        {0, sin(cameraPose.Rotation().Z().value()), cos(cameraPose.Rotation().Z().value())}
+    };
+    
+    //TODO: Make these constants
+    pitchMatrix = RotationMatrix{
+        {cos(cameraPose.Rotation().Y().value()), 0, sin(cameraPose.Rotation().Y().value())},
+        {0, 1, 0},
+        {-sin(cameraPose.Rotation().Y().value()), 0, cos(cameraPose.Rotation().Y().value())}
+    };
+
+    fullRotationMatrix = rollMatrix * yawMatrix * pitchMatrix;
+
+    //TODO: Make these constants
+    cameraTranslation = Eigen::Matrix<double, 3, 1>{
+        {cameraPose.X().value()},
+        {cameraPose.Y().value()},
+        {cameraPose.Z().value()}
+    };
 }
 
 frc::Pose3d GamePieceSensor::getGlobalPose() {
-
 
     if (!hasTarget()) return frc::Pose3d();
 
@@ -41,13 +71,37 @@ frc::Pose3d GamePieceSensor::getGlobalPose() {
     );
 }
 // clang-format on
-#define ANGLE_ERR 1.972405059
 void GamePieceSensor::updateRelative() {
+
+    //TODO: Make this an input and class memmber constant
+    Eigen::Matrix<double, 3, 3> intrinsicMatrix{
+        {1038.543, 0, 609.345},
+        {0, 1037.537, 469.070},
+        {0, 0, 1}
+    };
+
+    //TODO: Do this on init
+    Eigen::Matrix<double, 3, 4> CameraPoseMatrix{
+        {fullRotationMatrix.row(0)[0], fullRotationMatrix.row(0)[1], fullRotationMatrix.row(0)[2], cameraTranslation[0]},
+        {fullRotationMatrix.row(1)[0], fullRotationMatrix.row(1)[1], fullRotationMatrix.row(1)[2], cameraTranslation[1]},
+        {fullRotationMatrix.row(2)[0], fullRotationMatrix.row(2)[1], fullRotationMatrix.row(2)[2], cameraTranslation[2]},
+    };
+
+    auto inverseHomography = Eigen::Inverse(intrinsicMatrix * CameraPoseMatrix);
+    
     if (!hasTarget()) return;
-    double angle = cameraPose.Rotation().Y().value() + (units::angle::degree_t(ty).convert<units::angle::radian>().value() + (ANGLE_ERR * (M_PI/180.0)) /* * 1.36586656 */) /* + (((ty * M_PI) / 180.0) / 3.0) */; 
-    std::cout << "\n\n\t\t" << angle << "\n\n";
-    relativePoseFromCamera.x = cameraPose.Z() * tan((M_PI / 2.0) + angle);
-    relativePoseFromCamera.y = relativePoseFromCamera.x / tan((M_PI/2.0) - cameraPose.Rotation().Z().value() + units::angle::degree_t(tx).convert<units::angle::radian>().value());
+    
+    //TODO: make this suitable with enums for different types of limelights
+    Eigen::Matrix<double, 4, 1> pixelCoord {
+        {(62.5 - (2 * tx)) / (2 * 62.5)},
+        {(48.9 - (2 * ty)) / (2 * 48.9)},
+        {1}
+    };
+
+    Eigen::Matrix<double, 3, 1> gamePieceMatrix = inverseHomography * pixelCoord;
+
+    relativePoseFromCamera.x = units::meter_t{gamePieceMatrix.col(0)[0]};
+    relativePoseFromCamera.y = units::meter_t{gamePieceMatrix.col(0)[1]};
 
     updateRelativeToCenter();
 }
