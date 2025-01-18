7f9ee9db582c8c804545cc4fa0791cfd5f1b3d0c
Pearl Lam <pearllam@Pearls-MacBook-Pro.local>
2025-01-18 10:46:13 -0600

    feat: added current sensor

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -13,8 +13,11 @@
 #define CLIMB_MAX_SPEED units::angular_velocity::turns_per_second_t (0)
 #define CLIMB_MAX_ACCEL units::angular_acceleration::turns_per_second_squared_t (0)
 #define CLIMB_K_P 0
+#define CLIMB_K_D 0
 #define CLIMB_K_ERROR units::angle::turn_t (0)
+#define CLIMB_K_JERK units::angular_jerk::turns_per_second_cubed_t (0)
 #define CLIMB_K_AFF 0
+#define CLIMB_K_S 0
 
 #define CRAB_MAX_SPEED units::angular_velocity::turns_per_second_t (0)
 #define CRAB_MAX_ACCEL units::angular_acceleration::turns_per_second_squared_t (0)
@@ -30,15 +33,19 @@
 #define STOW_POS units::angle::turn_t (0)
 
 #define CRAB_SPEED units::voltage::volt_t (0)
+#define SPIKE_CURRENT 0
+#define CACHE_SIZE 0
 
 using namespace valor;
 
 Climber::Climber(frc::TimedRobot *_robot) : valor::BaseSubsystem(_robot, "Climber"),
-    climbMotor(nullptr)
+    climbMotor(nullptr),
+    crabMotor(nullptr),
+    currentSensor(_robot, "Climber")
 {
     frc2::CommandScheduler::GetInstance().RegisterSubsystem(this);
-    init();
     table->PutBoolean("Climbed?", state.climbed);
+    init();
 }
 
 Climber::~Climber()
@@ -50,52 +57,26 @@ void Climber::resetState()
 {
     state.climbState = CLIMB_STATE::STOW;
     state.crabState = CRAB_STATE::NO_CRAB;
+    currentSensor.reset();
 }
 
 void Climber::init()
 {
-    valor::PIDF climbPID;
-    climbPID.maxVelocity = CLIMB_MAX_SPEED;
-    climbPID.maxAcceleration = CLIMB_MAX_ACCEL;
-    climbPID.P = CLIMB_K_P;
-    climbPID.error = CLIMB_K_ERROR;
-    climbPID.aFF = CLIMB_K_AFF;
+    setClimberPID();
+    setCrabPID();
 
-    valor::PIDF crabPID;
-    crabPID.maxVelocity = CRAB_MAX_SPEED;
-    crabPID.maxAcceleration = CRAB_MAX_ACCEL;
-    crabPID.P = CRAB_K_P;
-    crabPID.error = CRAB_K_ERROR;
-    crabPID.aFF = CRAB_K_AFF;
+    state.spikeCurrent = SPIKE_CURRENT;
+    state.cacheSize = CACHE_SIZE;
 
-    climbPID.aFFType = valor::FeedForwardType::LINEAR;
-    bool climberInversion = false;
-    bool crabInversion = false;
-    climbMotor = new valor::PhoenixController(
-        valor::PhoenixControllerType::FALCON,
-        CANIDs::CLIMBER,
-        valor::NeutralMode::Brake,
-        climberInversion,
-        "baseCAN"
-    );
+    currentSensor.setSpikeSetpoint(state.spikeCurrent);
+    currentSensor.setGetter([this]() { return crabMotor->getCurrent().to<double>(); });
+    currentSensor.setSpikeCallback([this]() { state.crabState == CRAB_STATE::CRABBED;});
+    currentSensor.setCacheSize(state.cacheSize);
+    
+    table->PutNumber("Spike Current", state.spikeCurrent);
+    table->PutNumber("Cache Size", state.cacheSize);
 
-    climbMotor->setGearRatios(CLIMB_GEAR_RATIO, 1.0);
-    climbMotor->setPIDF(climbPID, 0);
-    climbMotor->setContinuousWrap(true);
-    climbMotor->applyConfig();
-
-    crabMotor = new valor::PhoenixController(
-        valor::PhoenixControllerType::FALCON,
-        CANIDs::CRABB,
-        valor::NeutralMode::Brake,
-        crabInversion,
-        "baseCAN"
-    );
-
-    crabMotor->setGearRatios(CRAB_GEAR_RATIO, 1.0);
-    crabMotor->setPIDF(crabPID, 0);
-    crabMotor->setContinuousWrap(true);
-    crabMotor->applyConfig();
+    resetState();
 }
 
 void Climber::assessInputs()
@@ -113,10 +94,16 @@ void Climber::assessInputs()
 
 void Climber::analyzeDashboard()
 {
+    state.spikeCurrent = table->GetNumber("Spike Current", SPIKE_CURRENT);
+    state.cacheSize = table->GetNumber("Cache Size", CACHE_SIZE);
+
+    currentSensor.setSpikeSetpoint(state.spikeCurrent);
+    currentSensor.setCacheSize(state.cacheSize);
+
     //crab sensor
-    /*if (crabSensor) {
+    if (state.crabState == CRAB_STATE::CRABBED) {
         state.climbState = CLIMB_STATE::CLIMB;
-    }*/
+    }
 
     if(climbMotor->getPosition() == CLIMBED_POS) {
         state.climbed = true;
@@ -143,6 +130,60 @@ void Climber::assignOutputs()
     }
 }
 
+void Climber::setClimberPID() {
+
+    valor::PIDF climbPID;
+
+    climbPID.maxVelocity = CLIMB_MAX_SPEED;
+    climbPID.maxAcceleration = CLIMB_MAX_ACCEL;
+    climbPID.P = CLIMB_K_P;
+    climbPID.D = CLIMB_K_D;
+    climbPID.error = CLIMB_K_ERROR;
+    climbPID.aFF = CLIMB_K_AFF;
+    climbPID.maxJerk = CLIMB_K_JERK;
+    climbPID.aFFType = valor::FeedForwardType::CIRCULAR;
+    climbPID.S = CLIMB_K_S;
+
+    bool climberInversion = false;
+
+    climbMotor = new valor::PhoenixController(
+        valor::PhoenixControllerType::FALCON,
+        CANIDs::CLIMBER,
+        valor::NeutralMode::Brake,
+        climberInversion,
+        "baseCAN"
+    );
+
+    climbMotor->setGearRatios(CLIMB_GEAR_RATIO, 1.0);
+    climbMotor->setPIDF(climbPID, 0);
+    climbMotor->setContinuousWrap(true);
+    climbMotor->applyConfig();
+}
+
+void Climber::setCrabPID() {
+
+    valor::PIDF crabPID;
+
+    crabPID.maxVelocity = CRAB_MAX_SPEED;
+    crabPID.maxAcceleration = CRAB_MAX_ACCEL;
+    crabPID.P = CRAB_K_P;
+
+    bool crabInversion = false;
+
+    crabMotor = new valor::PhoenixController(
+        valor::PhoenixControllerType::FALCON,
+        CANIDs::CRABB,
+        valor::NeutralMode::Brake,
+        crabInversion,
+        "baseCAN"
+    );
+
+    crabMotor->setGearRatios(CRAB_GEAR_RATIO, 1.0);
+    crabMotor->setPIDF(crabPID, 0);
+    crabMotor->setContinuousWrap(true);
+    crabMotor->applyConfig();
+}
+
 
 void Climber::InitSendable(wpi::SendableBuilder& builder)
 {
@@ -150,6 +191,16 @@ void Climber::InitSendable(wpi::SendableBuilder& builder)
     builder.AddBooleanProperty(
         "Climbed?",
         [this]{return state.climbed;},
+        nullptr
+    );
+    builder.AddDoubleProperty(
+        "Spike Current",
+        [this] {return state.spikeCurrent;},
+        nullptr
+    );
+    builder.AddDoubleProperty(
+        "Cache Size",
+        [this] {return state.cacheSize;},
         nullptr
     );
 }
