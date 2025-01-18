08ee587cd76f24b8f6ebfba5e43140540832405a
Anyastha <anascorner777@gmail.com>
2025-01-18 10:57:12 -0600

    initsendable, pid, fixed warnings

--- src/main/cpp/subsystems/Scorer.cpp
+++ src/main/cpp/subsystems/Scorer.cpp
@@ -11,15 +11,29 @@
 
 #include <frc/DriverStation.h>
 
+#define ELEV_MAX_SPEED units::angular_velocity::turns_per_second_t (0)
+#define ELEV_MAX_ACCEL units::angular_acceleration::turns_per_second_squared_t (0)
+#define ELEV_K_P 0
+#define ELEV_K_ERROR units::angle::turn_t (0)
+#define ELEV_K_AFF 0
+
+#define SCORER_MAX_SPEED units::angular_velocity::turns_per_second_t (0)
+#define SCORER_MAX_ACCEL units::angular_acceleration::turns_per_second_squared_t (0)
+#define SCORER_K_P 0
+
+
 #define CORAL_LEVEL_2_HEIGHT 1.5f  // Example height for Coral Level 2
 #define CORAL_LEVEL_3_HEIGHT 3.5f  // Example height for Coral Level 3
 #define CORAL_LEVEL_4_HEIGHT 5.5f  // Example height for Coral Level 4
 #define TROUGH_HEIGHT 0.0f        // Trough position (ground level)
 #define MOTOR_TO_SENSOR 1.0f
 #define SENSOR_TO_MECH 1.0f
+
 Scorer::Scorer(frc::TimedRobot *_robot, Drivetrain *_drive) :
     valor::BaseSubsystem(_robot, "Scorer"),
     drivetrain(_drive),
+    scorerDebounceSensor(_robot, "FeederBanner"),
+    elevatorMotor(new valor::PhoenixController(valor::PhoenixControllerType::KRAKEN_X60, CANIDs::ELEV_WHEEL, valor::NeutralMode::Coast, true)),
     scorerMotor(new valor::PhoenixController(valor::PhoenixControllerType::KRAKEN_X60, CANIDs::SCORER_WHEEL, valor::NeutralMode::Coast, true)),
     lidarSensor(_robot, "Front Lidar Sensor", CANIDs::FRONT_LIDAR_SENSOR)
     {
@@ -88,6 +102,30 @@ Scorer::Scorer(frc::TimedRobot *_robot, Drivetrain *_drive) :
 
     void Scorer::init()
     {
+
+        valor::PIDF elevatorPID;
+        elevatorPID.maxVelocity = ELEV_MAX_SPEED;
+        elevatorPID.maxAcceleration = ELEV_MAX_ACCEL;
+        elevatorPID.P =ELEV_K_P ;
+        elevatorPID.error = ELEV_K_ERROR;
+        elevatorPID.aFF = ELEV_K_AFF;
+        elevatorPID.aFFType = valor::FeedForwardType::LINEAR;
+
+        valor::PIDF scorerPID;
+        scorerPID.maxVelocity = SCORER_MAX_SPEED;
+        scorerPID.maxAcceleration = SCORER_MAX_ACCEL;
+        scorerPID.P = SCORER_K_P;
+        
+        scorerMotor->setPIDF(scorerPID, 0);
+
+    //     scorerDebounceSensor.setGetter([this] { return (!->GetInWindow()); });
+    //     scorerDebounceSensor.setRisingEdgeCallback([this] {
+
+    //         //TODO: RISING SENSOR 2 EDGE
+      
+        
+    // });
+
         resetState();
     }
 
@@ -96,7 +134,10 @@ Scorer::Scorer(frc::TimedRobot *_robot, Drivetrain *_drive) :
         if (driverGamepad == nullptr || !driverGamepad->IsConnected())
         return;
 
-        if ( driverGamepad->GetAButton()) {
+        if (operatorGamepad == nullptr || !operatorGamepad->IsConnected())
+        return;
+
+        if (driverGamepad->GetAButton()) {
             state.coralLevel = ELEV_LVL::TWO;
         } else if (driverGamepad->GetBButton() ) {
             state.coralLevel = ELEV_LVL::THREE;
@@ -125,20 +166,39 @@ Scorer::Scorer(frc::TimedRobot *_robot, Drivetrain *_drive) :
     }
 
     void Scorer::assignOutputs()
-{
-    units::length::meter_t targetHeightInMeters = coralHMap[state.coralLevel];
-    double wheelDiameter = 0.1;
-    // TODO: check whether chain or pulley system
-    double targetRotations = targetHeightInMeters.value() / (wheelDiameter * M_PI);  // Convert meters to rotations (turns)
-    scorerMotor->setPosition(units::turn_t(targetRotations));
-    
-}
+    {
+        units::length::meter_t targetHeightInMeters = coralHMap[state.coralLevel];
+        double wheelDiameter = 0.1;
+        // TODO: check whether chain or pulley system
+        double targetRotations = targetHeightInMeters.value() / (wheelDiameter * M_PI);  // Convert meters to rotations (turns)
+        scorerMotor->setPosition(units::turn_t(targetRotations));
+    }
 
 
     void Scorer::InitSendable(wpi::SendableBuilder& builder)
     {
 
         builder.SetSmartDashboardType("Subsystem");
+        builder.AddDoubleProperty(
+            "State Speed",
+            [this] { return state.scoringState; },
+            nullptr
+        );
+        builder.AddDoubleProperty(
+            "Scorer Vel",
+            [this] {return scorerMotor->getSpeed().to<double>();},
+            nullptr
+        );
+        builder.AddDoubleProperty(
+            "Scorer current",
+            [this] {return scorerMotor->getCurrent().to<double>();},
+            nullptr
+        );
+
+
+
+
+
 
     }
 
