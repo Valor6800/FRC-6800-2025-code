c3fda93b677e937c9d7d72d944e9b4edd08ec5c0
Harshid <harshid.kkunka@gmail.com>
2025-01-27 17:35:46 -0600

    implement canrange sensor with laserproximitysensor

--- src/main/cpp/valkyrie/sensors/CANRangeSensor.cpp
+++ src/main/cpp/valkyrie/sensors/CANRangeSensor.cpp
@@ -3,41 +3,22 @@
 using namespace valor;
 
 CANrangeSensor::CANrangeSensor(frc::TimedRobot *_robot, const char *name, int deviceId, std::string canbus) :
-    BaseSensor(_robot, name),
+    LaserProximitySensor<units::millimeter_t>(_robot, name),
     device(new ctre::phoenix6::hardware::CANrange(deviceId, canbus))
 {
-    wpi::SendableRegistry::AddLW(this, "CANrange Sensor", sensorName);
     device->ClearStickyFaults();
 
-    setGetter(
+    LaserProximitySensor<units::millimeter_t>::setGetter(
         [this] () {
-            std::optional<ctre::phoenix6::StatusSignal<units::length::meter_t>> measurement = device->GetDistance();
+            units::millimeter_t measurement = units::millimeter_t{device->GetDistance().GetValueAsDouble()};
             if(!isFaulting()){
-                return units::length::meter_t{measurement->GetValueAsDouble()};
+                return units::millimeter_t{measurement};
             }
-            return units::length::meter_t{-1};
+            return units::millimeter_t{-1};
         }
     );
 
     reset();
-}
-
-void CANrangeSensor::reset()
-{
-    prevState = 0_m;
-    currState = prevState;
-}
-
-void CANrangeSensor::calculate()
-{
-    prevState = currState;
-    units::meter_t update = getSensor();
-    if(update > 1_m) currState = update;
-}
-
-units::length::meter_t CANrangeSensor::getCANrangeDistance(){
-    if(!isFaulting()) return units::length::meter_t{device->GetDistance().GetValueAsDouble()};
-    return units::length::meter_t{-1};
 }
 
 bool CANrangeSensor::isFaulting(){
@@ -45,38 +26,4 @@ bool CANrangeSensor::isFaulting(){
             device->GetFault_Hardware().GetValue() || 
             device->GetFault_Undervoltage().GetValue() || 
             device->GetFault_UnlicensedFeatureInUse().GetValue());
-}
-
-void CANrangeSensor::InitSendable(wpi::SendableBuilder &builder){
-    builder.SetSmartDashboardType("Subsystem");
-    builder.AddDoubleProperty(
-        "Previous State (m)",
-        [this] {return prevState.to<double>();},
-        nullptr
-    );
-    builder.AddDoubleProperty(
-        "Curr State (m)",
-        [this] {return currState.to<double>();},
-        nullptr
-    );
-    builder.AddBooleanProperty(
-        "Fault: Boot During Enable",
-        [this] {return device->GetFault_BootDuringEnable().GetValue();},
-        nullptr
-    );
-    builder.AddBooleanProperty(
-        "Fault: Hardware",
-        [this] {return device->GetFault_Hardware().GetValue();},
-        nullptr
-    );
-    builder.AddBooleanProperty(
-        "Fault: Undervoltage",
-        [this] {return device->GetFault_Undervoltage().GetValue();},
-        nullptr
-    );
-    builder.AddBooleanProperty(
-        "Fault: Unlicensed Features",
-        [this] {return device->GetFault_UnlicensedFeatureInUse().GetValue();},
-        nullptr
-    );
 }
--- src/main/include/valkyrie/sensors/CANRangeSensor.h
+++ src/main/include/valkyrie/sensors/CANRangeSensor.h
@@ -1,28 +1,20 @@
 #pragma once
 
-#include "valkyrie/sensors/BaseSensor.h"
 #include <frc/TimedRobot.h>
 #include <units/length.h>
 #include <ctre/phoenix6/CANrange.hpp>
 #include <string>
+#include "LaserProximitySensor.h"
 
 namespace valor {
 
-class CANrangeSensor : public BaseSensor<units::length::meter_t>
+class CANrangeSensor : public LaserProximitySensor<units::millimeter_t>
 {
 public:
     CANrangeSensor(frc::TimedRobot *_robot, const char *name, int deviceId, std::string canbus);
 
-    void reset();
-
-    units::length::meter_t getCANrangeDistance();
-
-    void InitSendable(wpi::SendableBuilder &builder) override;
-
 private:
     ctre::phoenix6::hardware::CANrange *device;
-
-    void calculate();
 
     bool isFaulting();
 };
