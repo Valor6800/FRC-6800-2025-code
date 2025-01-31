7113af83fe7b816bc76111dbc39bfecbef52a6ab
Mau38 <maudiaz0109@gmail.com>
2025-01-31 14:12:57 -0600

    fix(Auto-Align): All controllers are relative to blue corner

--- src/main/cpp/valkyrie/drivetrain/Swerve.cpp
+++ src/main/cpp/valkyrie/drivetrain/Swerve.cpp
@@ -137,7 +137,7 @@ void Swerve<AzimuthMotor, DriveMotor>::analyzeDashboard()
     }
     // Rotational Speed calculations
     if (alignToTarget) {
-        rot_controller.SetGoal(units::radian_t{targetAngle + rotAlignOffset});
+        rot_controller.SetGoal(units::radian_t{targetAngle - rotAlignOffset});
         units::radian_t robotRotation = getCalculatedPose().Rotation().Radians();
         rot_controller.Calculate(robotRotation);
         rotSpeedRPS = units::radians_per_second_t{rot_controller.Calculate(robotRotation)} + rot_controller.GetSetpoint().velocity;
@@ -156,7 +156,7 @@ void Swerve<AzimuthMotor, DriveMotor>::analyzeDashboard()
 
     joystickVector = Eigen::Vector2d{xSpeed, ySpeed};
     double dotProduct = joystickVector.dot(MAKE_VECTOR(targetAngle));
-    joystickVector = -dotProduct * MAKE_VECTOR(targetAngle);
+    joystickVector = dotProduct * MAKE_VECTOR(targetAngle);
     joystickVector *= maxDriveSpeed.value();
     // joystickVector = Eigen::Vector2d{joystickVector[0], joystickVector[1]};
 
@@ -166,7 +166,7 @@ void Swerve<AzimuthMotor, DriveMotor>::analyzeDashboard()
         calculated_y_controller_val = y_controller.Calculate(yDistance, goalAlign);
         relativeToTagSpeed = units::meters_per_second_t{calculated_y_controller_val} + y_controller.GetSetpoint().velocity;
 
-        pidVector = MAKE_VECTOR((targetAngle + 2 * rotAlignOffset) - 90_deg) * relativeToTagSpeed.value();
+        pidVector = MAKE_VECTOR(targetAngle - rotAlignOffset) * relativeToTagSpeed.value();
         powerVector = joystickVector + pidVector;
         // powerVector *= dotProduct / fabs(dotProduct);
         xSpeedMPS = units::meters_per_second_t{powerVector[0]};
