9998a42513711eda698366ecbaa6724d565ad455
Harshid <harshid.kkunka@gmail.com>
2025-02-07 18:17:21 -0600

    find effective coeffcient of friction

--- src/main/cpp/valkyrie/drivetrain/TractionController.cpp
+++ src/main/cpp/valkyrie/drivetrain/TractionController.cpp
@@ -4,7 +4,7 @@
 #define MAX_SLIP_RATIO 0.40f
 #define SIGMOID_K 10
 #define FORCE_ACCELERATION_MULTIPLIER 0.7f
-#define INTERTIAL_VELOCITY_THRESHOLD 0.01_mps
+#define INERTIAL_VELOCITY_THRESHOLD 0.01_mps
 #define VELOCITY_DIFFERENCE_THRESHOLD 1.0_mps
 #define MIN_SLIPPING_TIME 0.5_s
 #define VELOCITY_REQUEST_THRESHOLD 0.05_mps
@@ -39,7 +39,29 @@ TractionController::TractionController(units::dimensionless::dimensionless_t sta
 }
 
 units::meters_per_second_t TractionController::calculate(units::meters_per_second_t velocityRequest, units::meters_per_second_t inertialVelocity, units::meters_per_second_t wheelSpeed){
+    units::meters_per_second_t velocityOutput = velocityRequest;
+    bool oppositeDirection = inertialVelocity < 0_mps;
+
+    wheelSpeed = units::meters_per_second_t{std::abs(wheelSpeed.value())};
+    inertialVelocity = units::meters_per_second_t{std::abs(inertialVelocity.value())};
+
+    bool slowWheel = (std::abs(velocityRequest.value()) - wheelSpeed.value()) > VELOCITY_DIFFERENCE_THRESHOLD.value();
+    if(slowWheel) {_forceAccelerationTimer.Start();}
+    else {_forceAccelerationTimer.Reset(); _forceAccelerationTimer.Stop();}
     
+    bool forceAcceleration = _staticForceAccelerationDebouncer.Calculate(velocityRequest > VELOCITY_REQUEST_THRESHOLD && wheelSpeed < WHEEL_SPEED_THRESHOLD) || 
+                             _dyamicForceAccelerationDebouncer.Calculate(slowWheel && !_forceAccelerationTimer.AdvanceIfElapsed(FORCE_ACCELERATE_TIME) && !oppositeDirection);
+    
+    double currentSlipRatio = (inertialVelocity < INERTIAL_VELOCITY_THRESHOLD) ? (wheelSpeed.value()/_maxLinearVelocity) : 
+                                                                                 (std::abs(wheelSpeed.value())) - inertialVelocity.value() / inertialVelocity.value();
+
+    _isSlipping = _slippingDebouncer.Calculate(currentSlipRatio > _optimalSlipRatio && std::abs(wheelSpeed.value()) > _maxLinearVelocity * _optimalSlipRatio && isEnabled());
+    
+    units::meters_per_second_squared_t desiredAcceleration = units::meters_per_second_squared_t{(velocityRequest.value() - (inertialVelocity.value() * oppositeDirection ? -1 : 1)) / 0.02};
+
+    double sigmoid = 1 / (1 + std::exp(-SIGMOID_K * std::clamp(2 * (currentSlipRatio - _optimalSlipRatio) - 1, -1.0, 1.0)));
+
+    double effectiveCOF = _isSlipping ? _staticCOF * (1 - sigmoid) + _dynamicCOF * sigmoid : _staticCOF;
 }
 
 bool TractionController::isModuleSlipping(){
