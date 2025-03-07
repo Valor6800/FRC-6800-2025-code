fe939f0c37978c4cc7eef0e78d580ad18a5fc16f
Mau38 <maudiaz0109@gmail.com>
2025-03-07 10:08:22 -0600

    fix: Re-zeroed abs magnet offset for the elevator cancoder

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -255,7 +255,7 @@ namespace Constants {
 
         static units::angle::turn_t getElevatorMagnetOffset() { switch (robot) {
             case Robot::Alpha: return 0.10327_tr; 
-            case Robot::Gold: return 0.989014_tr;
+            case Robot::Gold: return 0.652344_tr; //  0.989014_tr;
             default: return 0.998_tr;
         }};
 
