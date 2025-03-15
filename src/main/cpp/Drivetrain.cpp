6fdddb54183852d4a36bc0df25f0ef9fb4b641fb
Valor 6800 <frcviperbots@gmail.com>
2025-03-15 16:14:48 -0500

    fix: Endgame rumble to 15 secs instead of 30 secs

--- src/main/cpp/Drivetrain.cpp
+++ src/main/cpp/Drivetrain.cpp
@@ -58,7 +58,7 @@ const units::meter_t WHEEL_DIAMETER(0.0973_m);
 #define VISION_ACCEPTANCE 3.5_m // meters
 
 // 135 - TELEOP_MAX_TIME - TIME_TO_RUMBLE = Time at which rumble starts
-#define TIME_TO_RUMBLE 30_s
+#define TIME_TO_RUMBLE 15_s
 #define TELEOP_MAX_TIME 135_s
 
 #define MT2_POSE true
