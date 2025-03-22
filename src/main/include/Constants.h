1df6677dca5b3b16e2a864949785333c838d5260
Ana Bolshakova <anascorner777@gmail.com>
2025-03-21 19:40:37 -0500

    zeroing

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -266,7 +266,7 @@ namespace Constants {
         static units::angle::turn_t getElevatorMagnetOffset() { switch (robot) {
             case Robot::Alpha: return 0.10327_tr; 
             case Robot::Gold: return 0.203369_tr; //  0.989014_tr;
-            default: return 0.480713_tr;
+            default: return -0.07860_tr;
         }};
 
         static double getElevKAFF() { switch (robot) {
