cfab5c8a20c1b6c94cbd16961f3256a2340ae290
Michael Ray <mray190@gmail.com>
2025-03-21 23:18:32 -0500

    feat: Turn on xController in teleop

--- src/main/cpp/valkyrie/drivetrain/Swerve.cpp
+++ src/main/cpp/valkyrie/drivetrain/Swerve.cpp
@@ -241,7 +241,7 @@ void Swerve<AzimuthMotor, DriveMotor>::analyzeDashboard()
         yAlignVector = MAKE_VECTOR(targetAngle - 90_deg) * relativeToTagSpeed.value();
         if(xAlign) {
             if (dumbAutoAlign)
-                xAlignVector = MAKE_VECTOR(targetAngle) * 0.75;
+                xAlignVector = MAKE_VECTOR(targetAngle) * 1.0;
             else
                 xAlignVector = MAKE_VECTOR(targetAngle) * -relativeToTagXSpeed.value();
             powerVector = yAlignVector + xAlignVector;
