0e4d3c8faf66387e7e5e6132df255885a8349282
Michael Ray <mjray@amazon.com>
2025-03-28 21:46:26 -0500

    cancoder again

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -265,7 +265,7 @@ namespace Constants {
 
         static units::angle::turn_t getElevatorMagnetOffset() { switch (robot) {
             case Robot::Alpha: return 0.10327_tr;
-            case Robot::Gold: return 0.1528322031_tr; //  0.989014_tr;
+            case Robot::Gold: return 0.3200678906_tr; //  0.989014_tr;
             default: return 0.278809_tr;
         }};
 
