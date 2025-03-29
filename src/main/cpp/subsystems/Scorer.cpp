028ff7696568f54d5f883039c7020b11d1e98915
Mau38 <maudiaz0109@gmail.com>
2025-03-29 15:54:39 -0500

    feat: Reduced auto dunk max speed to .75mps

--- src/main/cpp/subsystems/Scorer.cpp
+++ src/main/cpp/subsystems/Scorer.cpp
@@ -36,7 +36,7 @@
 #define VIABLE_ELEVATOR_THRESHOLD 0.02_m
 #define VIABLE_DUNK_DISTANCE 0.28_m
 #define VIABLE_ELEVATOR_DISTANCE 1.8_m //consider the offset of the canrange from the front of robot which is 8 inches
-#define VIABLE_DUNK_SPEED_L4 1.1_mps
+#define VIABLE_DUNK_SPEED_L4 0.75_mps
 
 #define MIN_DUNK_DISTANCE_OVER_CORAL 11_in
 #define MAX_DUNK_DISTANCE_OVER_CORAL 14.5_in
