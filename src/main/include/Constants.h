ae3a500ec612efa52f165c9610c1eee427378651
Harshid <harshid.kkunka@gmail.com>
2025-03-31 18:57:52 -0500

    fix: make robot come further to hp from b-hp after auto align

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -265,7 +265,7 @@ namespace Constants {
 
         static units::angle::turn_t getElevatorMagnetOffset() { switch (robot) {
             case Robot::Alpha: return 0.10327_tr;
-            case Robot::Gold: return 0.8334965312_tr; //  0.989014_tr;
+            case Robot::Gold: return -0.0622559063_tr; //  0.989014_tr;
             default: return -0.303710_tr;
         }};
 
