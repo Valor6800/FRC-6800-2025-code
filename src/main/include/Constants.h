c8ee2e5bd47f4693332dd96c9f0a95682c17975f
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-09-13 11:12:47 -0500

    Changed elev offset

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -287,7 +287,7 @@ namespace Constants {
         */
         static units::angle::turn_t getElevatorMagnetOffset() { switch (robot) {
             case Robot::Alpha: return 0.10327_tr;
-            case Robot::Gold: return 0.700_tr; // Phoenix Tuner: -.218017578125
+            case Robot::Gold: return .118652_tr; // Phoenix Tuner: -.218017578125
             default: return 0.764893_tr;
         }};
 
